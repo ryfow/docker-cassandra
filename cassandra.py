31c350125533d7c5fb7e4970604f4502ae0c80e0
Ryan Fowler <ryan.fowler@singlewire.com>
2014-05-04 12:45:52 -0500

    fix quoting problem and default seeds to 127.0.0.1

--- cassandra.py
+++ cassandra.py
@@ -8,8 +8,6 @@ def go():
   f = open('/apache-cassandra/conf/cassandra.yaml', 'w')
   substitution_map = os.environ.copy()
   # TODO: munge map
-  if substitution_map['SEEDS'] == '':
-    substitution_map['SEEDS'] = '127.0.0.1'
   if not 'INITIAL_TOKEN' in substitution_map:
     substitution_map['INITIAL_TOKEN'] = ''
 
