f75beb5a591366730e34cfd27dfa1ddda629e770
CoreOS Admin <core@ip-10-160-149-176.us-west-1.compute.internal>
2014-05-05 15:49:43 +0000

    Fleet of one actually comes up

--- cassandra.py
+++ cassandra.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 from string import Template
+import socket
 import os
 def go():
   print("Starting Cassandra")
   f = open('/apache-cassandra/conf/cassandra.yaml.template', 'r')
   s = Template(f.read())
   f = open('/apache-cassandra/conf/cassandra.yaml', 'w')
-  substitution_map = os.environ.copy()
-  # TODO: munge map
-  if not 'INITIAL_TOKEN' in substitution_map:
-    substitution_map['INITIAL_TOKEN'] = ''
+  submap = os.environ.copy()
 
-  f.write(s.substitute(substitution_map))
+  if not 'INITIAL_TOKEN' in submap:
+    submap['INITIAL_TOKEN'] = ''
+  if 'PUBLIC_NAME' in submap:
+    submap['BROADCAST_ADDRESS'] = socket.gethostbyname(submap['PUBLIC_NAME'])
+
+  submap['LISTEN_ADDRESS'] = socket.gethostbyname(socket.gethostname())
+  submap['SEEDS'] = submap['BROADCAST_ADDRESS']
+  submap['RPC_ADDRESS'] = "0.0.0.0"
+  f.write(s.substitute(submap))
   os.system("/apache-cassandra/bin/cassandra -f")
 
 if __name__ == '__main__':
