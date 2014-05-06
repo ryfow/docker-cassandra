dfc1c6b4ac0bcf1555aa9af4bfdc652c1607defb
Ryan Fowler <ryan.fowler@singlewire.com>
2014-05-06 11:18:13 -0500

    two node cluster
    switched to ubuntu 12.04 because I couldn't get pip to work on 14.04
    INITIAL_NODE needs to be set on the first cluster member
    Hopefully this'll extend to three nodes easily enough.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM ubuntu:14.04
+FROM ubuntu:12.04
 MAINTAINER Ryan Fowler <ryan.fowler@singlewire.com>
 
 RUN apt-get update -y
 RUN apt-get install -y --no-install-recommends openjdk-7-jdk openjdk-7-jre
-RUN apt-get install -y curl vim python
+RUN apt-get install -y curl vim python-pip libssl-dev
+RUN apt-get install -y python-openssl python-dev
+RUN pip install python-etcd
 
 RUN curl -O "http://archive.apache.org/dist/cassandra/1.2.16/apache-cassandra-1.2.16-bin.tar.gz"
 
@@ -20,9 +22,7 @@ ENV SEEDS 127.0.0.1
 ENV CLUSTER_NAME "Test Cluster"
 ENV LISTEN_ADDRESS 127.0.0.1
 ENV BROADCAST_ADDRESS 127.0.0.1
-ENV RPC_ADDRESS 127.0.0.1
+ENV RPC_ADDRESS 0.0.0.0
 ENV NUM_TOKENS 256
 
 CMD ["/cassandra.py"]
-
-
--- cassandra.py
+++ cassandra.py
@@ -2,6 +2,8 @@
 from string import Template
 import socket
 import os
+import etcd
+import json
 def go():
   print("Starting Cassandra")
   f = open('/apache-cassandra/conf/cassandra.yaml.template', 'r')
@@ -15,8 +17,16 @@ def go():
     submap['BROADCAST_ADDRESS'] = socket.gethostbyname(submap['PUBLIC_NAME'])
 
   submap['LISTEN_ADDRESS'] = socket.gethostbyname(socket.gethostname())
-  submap['SEEDS'] = submap['BROADCAST_ADDRESS']
-  submap['RPC_ADDRESS'] = "0.0.0.0"
+
+  if 'INITIAL_NODE' in submap and submap['INITIAL_NODE'] == "true":
+    submap['SEEDS'] = submap['BROADCAST_ADDRESS']
+  else:
+    client = etcd.Client(host=submap['PUBLIC_NAME'])
+    cas1_json = client.get("/services/cassandra/cassandra1").value
+    cas1_dict = json.loads(cas1_json)
+    cas1_host = cas1_dict['host']
+    submap['SEEDS'] = socket.gethostbyname(cas1_host)
+
   f.write(s.substitute(submap))
   os.system("/apache-cassandra/bin/cassandra -f")
 
